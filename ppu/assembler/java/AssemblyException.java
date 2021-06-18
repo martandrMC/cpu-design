78ac6ae569834c3aadff786c449fc0f5e8ffb8e9
martandrMC <martin.andronikos@gmail.com>
2021-06-18 21:02:47 +0300

    Upload PPU Assembler Code (Java)
    The files contain the java code for the assembler used to compile the examples for the processor.
    Afterwards, the binaries are to be imported in the program memory of the processor.

---
+++ ppu/assembler/java/AssemblyException.java
@@ -0,0 +1,6 @@
+public class AssemblyException extends Exception {
+	
+	private static final long serialVersionUID = 264711272548091676L;
+	public AssemblyException(String msg) { super(msg); }
+	public AssemblyException() { super(); }
+}
